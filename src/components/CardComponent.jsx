d370bfb5fd45e428b7d2d634f53d25c9d1713045
Jacob Mayeux <jacobmayeux@gmail.com>
2020-05-21 04:59:00 -0500

    Added img to cards page if no search is conducted, fixed bug for showing online cards, fixed styling

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -12,19 +12,25 @@ function CardComponent({cardInfo}){
     
     if(cardInfo.card_faces && cardInfo.card_faces[0].image_uris){           //For flip cards
         cardImagesSrcs = [cardInfo.card_faces[0].image_uris.normal, cardInfo.card_faces[1].image_uris.normal]
-        cardImage = <input className="card-img-top" type="image" src={cardInfo.card_faces[0].image_uris.normal} onClick={handleOnClick}/>
+        cardImage = <input className="card-img-top" type="image" src={cardInfo.card_faces[0].image_uris.normal} onClick={handleOnClick} alt={cardInfo.name}/>
     }
     else{
         cardImage = <img src={cardInfo.image_uris.normal} className="card-img-top" alt={cardInfo.name}/>
     }
 
     return ( 
-        <div className="card m-1 border border-dark bg-dark rounded d-inline-flex" style={{width: "18rem"}}>
+        <div className="card m-2 p-1 border border-success bg-dark rounded d-inline-flex" style={{width: "15rem"}}>
             {cardImage}
-            <h5 className="text-primary text-center">{cardInfo.set_name}</h5>
-            <div className="card-body row">
-                <p className="card-subtitle card-text text-primary col-sm">Price: ${cardInfo.prices.usd}</p>
-                <p className="card-subtitle card-text text-primary col-sm">Foil: ${cardInfo.prices.usd_foil}</p>
+            <div className="card-body">
+                <div className="row d-flex justify-content-center">
+                    <h5 className="text-primary text-center">{cardInfo.set_name}</h5>
+                </div>
+                <div className="row d-flex justify-content-start">
+                    {cardInfo.prices.usd ? <p className="card-subtitle card-text text-primary col-sm">Price: ${cardInfo.prices.usd}</p> : null}
+                </div>
+                <div className="row d-flex justify-content-start">
+                    {cardInfo.prices.usd_foil ? <p className="card-subtitle card-text text-primary col-sm">Foil: ${cardInfo.prices.usd_foil}</p> : null}
+                </div>
             </div>
         </div>
     )
--- src/components/CardsComponent.jsx
+++ src/components/CardsComponent.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import CardComponent from './CardComponent'
+import VampiricTutor from '../multimedia/Vampiric-tutor.jpg'
 
 function CardsComponent({cardList}){
     console.log("TESTING: CardsComponent Render")
     let key = 0
     let cards = cardList !== undefined ? cardList.map(function(cardInfo){
-        // console.log(cardInfo)
-        if(!cardInfo.digital)       //Do not show online only versions of the card
-            key++
-            return <CardComponent cardInfo={cardInfo} key={key}/>
-    })
-    :
-    ''
-
+                                                if(!cardInfo.digital){       
+                                                    key++
+                                                    return <CardComponent cardInfo={cardInfo} key={key}/>
+                                                }
+                                            }) : null
+    console.log("CardList", cardList)
+    if(!cards.length){
+        cards = <div>
+                    <h3 className="text-primary">Search for a card</h3>
+                    <img className="w-25" src={VampiricTutor}/>
+                </div>
+    }                                  
     return (         
         <div align="center" className="justify-content-center mt-3" style={{backgroundColor: "black"}}>
-            {
-                cards
-            }
+            { cards }
         </div>
     )
 }
