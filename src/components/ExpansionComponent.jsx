e202ba71603d7a8ff256308d36a43b76a9dd1374
Jacob <jacobmayeux@ibm.com>
2020-05-28 20:28:45 -0500

    Added progress bar to expansions cards when user logged in

--- src/components/ExpansionComponent.jsx
+++ src/components/ExpansionComponent.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 
-function ExpansionComponent({expansionName, expansionIcon, expansionCode, onSetClicked, setNavTitle, setIsFromSet}){
+function ExpansionComponent({expansionInfo, onSetClicked, setNavTitle, setIsFromSet, isUserLogin}){
     const history = useHistory()
+    console.log(expansionInfo)
     
     const handleOnClick = (e) => {
         e.preventDefault()
         setIsFromSet(true)
-        onSetClicked(expansionCode)
-        setNavTitle(`Do I Have Cards From: ${expansionName.toUpperCase()}`)
+        onSetClicked(expansionInfo.code)
+        setNavTitle(`Do I Have Cards From: ${expansionInfo.name.toUpperCase()}`)
         history.push('/cards')
     }
 
     return ( 
         <div className="card m-2 p-1 border border-success bg-dark rounded col-md-2" onClick={handleOnClick} style={{cursor: "pointer"}}>
             <div align="center">
-                <img src={expansionIcon} align="center" className="card-img-top" alt={expansionName} style={{height: 100, width: 100}}/>
+                <img src={expansionInfo.icon_svg_uri} align="center" className="card-img-top" alt={expansionInfo.name} style={{height: 100, width: 100}}/>
             </div>
             <div className="card-body justify-content-center">
-                <p className="text-primary text-center">{expansionName}</p>
+                <h5 className="text-primary text-center">{expansionInfo.name}</h5>
             </div>
+            {isUserLogin ? 
+                <div>
+                    <div className="d-flex justify-content-around">
+                        <p className="text-success">Owned: </p>
+                        <p className="text-success">Total: {expansionInfo.card_count}</p>
+                    </div>  
+                    <div className="progress" style={{height: 20}}>
+                        <div className="progress-bar bg-primary" role="progressbar" style={{width: "50%"}} aria-valuemin="0" aria-valuemax="100">50%</div>
+                    </div>
+                </div> : null
+            }
         </div>
     )
 }
--- src/components/SetComponent.jsx
+++ src/components/SetComponent.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 import ExpansionComponent from './ExpansionComponent'
 
-function SetComponent({ expansionList, onSetClicked, setNavTitle,setIsFromSet }){
+function SetComponent({ expansionList, onSetClicked, setNavTitle, setIsFromSet, isUserLogin }){
     console.log("TESTING: SetComponent Render")
 
     if(expansionList !== undefined && expansionList.length){
         return ( 
             <div align="center" className="justify-content-center mt-3 mb-5" style={{backgroundColor: "black", display: "flex", flexWrap: "wrap"}}>
                 {expansionList.map(function(expansion){
-                    if(!expansion.digital)
-                        return <ExpansionComponent expansionName={expansion.name} expansionIcon={expansion.icon_svg_uri} key={expansion.code} expansionCode={expansion.code} onSetClicked={onSetClicked} setNavTitle={setNavTitle} setIsFromSet={setIsFromSet}/> 
+                    if(!expansion.digital){
+                        return <ExpansionComponent 
+                                    expansionInfo={expansion} 
+                                    onSetClicked={onSetClicked} 
+                                    setNavTitle={setNavTitle} 
+                                    setIsFromSet={setIsFromSet} 
+                                    isUserLogin={isUserLogin}/> 
+                    }
                 }) }
             </div>
         )
