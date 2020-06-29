5f7246a8cca81bc5b3555c3b6f299438dbfb4a2d
Jacob Mayeux <jacobmayeux@gmail.com>
2020-06-29 06:00:29 -0500

    Refactored code - changed variable naming convention

--- src/components/ExpansionComponent.jsx
+++ src/components/ExpansionComponent.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
-function ExpansionComponent({ expansionInfo, userEmail }){
-    const history = useHistory()
-    const counter = useSelector(state => state.currentUser)
+function ExpansionComponent({ oExpansionInfo }){
+    const fHistory = useHistory()
+    const oUserInfo = useSelector(state => state.currentUser)
     
-    const handleOnClick = (e) => {
-        e.preventDefault()
+    const handleOnClick = (event) => {
+        event.preventDefault()
         // setIsFromSet(true)
-        // onSetClicked(expansionInfo.code)
-        // setNavTitle(`Do I Have Cards From: ${expansionInfo.name.toUpperCase()}`)
+        // onSetClicked(oExpansionInfo.code)
+        // setNavTitle(`Do I Have Cards From: ${oExpansionInfo.name.toUpperCase()}`)
 
         // fetch(`https://api.scryfall.com/cards/search?order=set&q=e%3A${setClicked}&unique=prints`)
         //     .then(response => response.json())
@@ -23,24 +23,22 @@ function ExpansionComponent({ expansionInfo, userEmail }){
         //             setCardList(data.data)
         //         }
         //     });
-        
-        console.log(counter)
-        // history.push('/cards')
+        // fHistory.push('/cards')
     }
 
     return ( 
         <div className="card m-2 p-1 border border-success bg-dark rounded col-md-2" onClick={handleOnClick} style={{cursor: "pointer"}}>
             <div align="center">
-                <img src={expansionInfo.icon_svg_uri} align="center" className="card-img-top" alt={expansionInfo.name} style={{height: 100, width: 100}}/>
+                <img src={oExpansionInfo.icon_svg_uri} align="center" className="card-img-top" alt={oExpansionInfo.name} style={{height: 100, width: 100}}/>
             </div>
             <div className="card-body justify-content-center">
-                <h5 className="text-primary text-center">{expansionInfo.name}</h5>
+                <h5 className="text-primary text-center">{oExpansionInfo.name}</h5>
             </div>
-                {userEmail ? 
+                {oUserInfo.userEmail ? 
                         <div>
                             <div className="d-flex justify-content-around">
                                 <p className="text-success">Owned: </p>
-                                <p className="text-success">Total: {expansionInfo.card_count}</p>
+                                <p className="text-success">Total: {oExpansionInfo.card_count}</p>
                             </div>  
                             <div className="progress" style={{height: 20}}>
                                 <div className="progress-bar bg-primary" role="progressbar" style={{width: "50%"}} aria-valuemin="0" aria-valuemax="100">50%</div>
