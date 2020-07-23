2bf0f4b4544ec8da48e7a311129c93d85bdb5a0f
Jacob Mayeux <jacobmayeux@gmail.com>
2020-07-23 05:39:59 -0500

    Added fixed conflicted file

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -2,16 +2,10 @@
 import { GET_CURRENT_USER } from '../actions/types'
 
 const initialState = {
-<<<<<<< HEAD
-    sUserEmail: null, 
-    nCollectionId: null,
-    bIsLoggedIn: false
-=======
     sEmailAddress: null, 
     sUserCollectionId: null,
     bIsLoggedIn: false,
     bIsLoading: false
->>>>>>> devBranch
 }
 
 export default function currentUser(state = initialState, action) {
